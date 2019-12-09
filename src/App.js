0d17ae2ce0c9ca90566119306fbecafcbc7efa90
Gayasuddin Siddiqui <gayasuddin93@gmail.com>
2019-12-09 19:00:45 +0530

    Added collection-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
+import ShopPage from './pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.component'
 
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
+    </div>
   );
 }
 
