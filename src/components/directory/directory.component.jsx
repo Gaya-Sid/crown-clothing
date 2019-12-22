7bafffd2c4f2b1cb35bf19cc6810e9205f7b96e1
Gayasuddin Siddiqui <gayasuddin93@gmail.com>
2019-12-22 20:38:57 +0530

    minor changes in directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
 import './directory.styles.scss';
 
 import MenuItem from '../menu-item/menu-item.component';
